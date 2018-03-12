296ffc7cdc8d930b24956b348500073216177a80
halsaied <5078@H.Alsaied>
2018-03-12 18:54:16 +0100

    another modification

--- src/extraction.py
+++ src/extraction.py
@@ -135,14 +135,14 @@ def extractDistance(trans):
                 s0Idx = sent.tokens.index(stackTokens[-1])
                 featSet.add('Distance(S0,B0)={0}'.format(b0Idx - s0Idx))
         if len(config.stack) > 1:
-            print trans
+            #print trans
             s0Tokens = getTokens(config.stack[-1])
             s1Tokens = getTokens(config.stack[-1])
             if v2featureSettings.useS0S1Distance:
                 s0Idx = sent.tokens.index(s0Tokens[0])
                 s1Idx = sent.tokens.index(s1Tokens[-1])
                 featSet.add('Distance(S0,S1)={0}'.format(s0Idx - s1Idx))
-                print 'Distance(S0,S1)={0}'.format(s0Idx - s1Idx)
+                #print 'Distance(S0,S1)={0}'.format(s0Idx - s1Idx)
     return featSet
 
 
